249671fea4f1264976217076bc1ef66b6e3f4139
Ekene Godswill <godswillekene884@gmail.com>
2026-05-12 22:52:54 +0100

    login modified

--- projects/login_system2.py
+++ projects/login_system2.py
@@ -1,5 +1,3 @@
-
-
 def load_users():
     users = {}
 
@@ -16,14 +14,21 @@ def save_user(username, password):
         file.write(f"{username},{password}\n")
 
 
-def register(users , username, password):
+def register(users, username, password):
     if username in users:
         return "Username already exists!"
-    
-    else:
-        save_user(username, password)
-        return "User registered successfully"
-    
+
+    save_user(username, password)
+    return "User registered successfully"
+
+
+def reduce_attempts(attempts):
+    attempts -= 1
+    print(f"Attempts left: {attempts}")
+
+    if attempts == 1:
+        print("Suspicious activity detected 🚨")
+
 
 def login_system():
     attempts = 3
@@ -31,35 +36,32 @@ def login_system():
     while attempts > 0:
         users = load_users()
 
-        action = input("Register(new user) / Login(existing user)?\n ").strip().lower()
+        action = input("Register(new user) / Login(existing user)?\n").strip().lower()
         username = input("Enter username:\n").strip().lower()
         password = input("Enter password:\n").strip()
 
         if action == "register":
             message = register(users, username, password)
             print(message)
-            
+
         elif action == "login":
+
             if username not in users:
                 print("User not found ❌")
-                continue
+                attempts = reduce_attempts(attempts)
 
             elif users[username] != password:
                 print("Wrong password ❌")
-                continue
+                attempts = reduce_attempts(attempts)
 
             else:
                 print("Access granted ✅")
                 return
-            
+
         else:
             print("Invalid option")
-
-        attempts -= 1
-        print(f"Attempts left: {attempts}")
-
-        if attempts == 1:
-            print("Suspicious activity detected 🚨")
+            
+        attempts = reduce_attempts(attempts)
 
     print("Account locked 🔒")
 
